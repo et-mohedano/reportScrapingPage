3930256657d18ff7d615bb71610ccd81f96fb334
Efrain Mohedano <et.mohedano@gmail.com>
2022-08-19 02:42:13 -0500

    work areas added to pdf lists

--- js/main.js
+++ js/main.js
@@ -1,8 +1,116 @@
 
-var list_of_secretaries = [{value: 0, text: "Salud"}, {value: 1, text: "Educación"}]
-var list_of_health_secretary = [{value: "Julio Menchaca_11_08_2022__18_41_18", text: "Julio Menchaca_11_08_2022__18_41_18"}, {value: "Julio Menchaca_11_08_2022__20_27_58", text: "Julio Menchaca_11_08_2022__20_27_58"}]
-var list_of_education_secretary = [{value: "Julio Menchaca_11_08_2022__20_27_58", text: "Julio Menchaca_11_08_2022__20_27_58"}, {value: "Julio Menchaca_11_08_2022__18_41_18", text: "Julio Menchaca_11_08_2022__18_41_18"}]
-
+var list_of_secretaries = [
+    {value: 0, text: "CONTRALORIA"}, 
+    {value: 1, text: "GENERAL"},
+    {value: 2, text: "SA"},
+    {value: 3, text: "SECTUR"},
+    {value: 4, text: "SECULTAH"},
+    {value: 5, text: "SEDAGRO"},
+    {value: 6, text: "SEDECO"},
+    {value: 7, text: "SEDSO"},
+    {value: 8, text: "SEFIN"},
+    {value: 9, text: "SEGOBH"},
+    {value: 10, text: "SEMARNATH"},
+    {value: 11, text: "SEMOTH"},
+    {value: 12, text: "SEP"},
+    {value: 13, text: "SEPPEH"},
+    {value: 14, text: "SOPOT"},
+    {value: 15, text: "STPS"}
+]
+var list_of_health_CONTRALORIA = [
+    {value: "Julio Menchaca_18_08_2022__12_26_51", text: "Reporte 1"}
+]
+var list_of_education_GENERAL = [
+    {value: "Julio Menchaca_17_08_2022__16_19_26", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_17_08_2022__16_25_31", text: "Reporte 2"},
+    {value: "Julio Menchaca_17_08_2022__16_27_47", text: "Reporte 3"},
+    {value: "Julio Menchaca_17_08_2022__16_33_26", text: "Reporte 4"},
+    {value: "Julio Menchaca_17_08_2022__16_39_38", text: "Reporte 5"},
+    {value: "Julio Menchaca_17_08_2022__16_44_14", text: "Reporte 6"},
+    {value: "Julio Menchaca_17_08_2022__16_46_40", text: "Reporte 7"},
+    {value: "Julio Menchaca_17_08_2022__16_59_37", text: "Reporte 8"},
+    {value: "Julio Menchaca_17_08_2022__17_03_51", text: "Reporte 9"},
+    {value: "Julio Menchaca_17_08_2022__17_08_36", text: "Reporte 10"},
+    {value: "Julio Menchaca_17_08_2022__17_12_50", text: "Reporte 11"},
+    {value: "Julio Menchaca_17_08_2022__17_17_12", text: "Reporte 12"},
+    {value: "Julio Menchaca_17_08_2022__17_23_20", text: "Reporte 13"},
+    {value: "Julio Menchaca_17_08_2022__17_29_40", text: "Reporte 14"},
+    {value: "Julio Menchaca_17_08_2022__17_36_57", text: "Reporte 15"}
+]
+var list_of_SA = [
+    {value: "Julio Menchaca_18_08_2022__11_51_53", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_18_08_2022__11_57_37", text: "Reporte 2"}
+]
+var list_of_SECTUR = [
+    {value: "Julio Menchaca_18_08_2022__12_38_02", text: "Reporte 1"}, 
+    {value: "nan_18_08_2022__15_51_34", text: "Reporte 2"}
+]
+var list_of_SECULTAH = [
+    {value: "Julio Menchaca_18_08_2022__12_47_00", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_18_08_2022__12_50_17", text: "Reporte 2"}
+]
+var list_of_SEDAGRO = [
+    {value: "Julio Menchaca_18_08_2022__14_15_51", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_18_08_2022__14_20_54", text: "Reporte 2"}
+]
+var list_of_SEDECO = [
+    {value: "Julio Menchaca_18_08_2022__12_03_01", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_18_08_2022__12_06_20", text: "Reporte 2"},
+    {value: "Julio Menchaca_18_08_2022__12_44_17", text: "Reporte 3"}, 
+    {value: "Julio Menchaca_18_08_2022__13_19_14", text: "Reporte 4"},
+    {value: "Julio Menchaca_18_08_2022__13_44_24", text: "Reporte 5"}, 
+    {value: "Julio Menchaca_18_08_2022__23_32_25", text: "Reporte 6"}
+]
+var list_of_SEDSO = [
+    {value: "Julio Menchaca_18_08_2022__13_29_53", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_18_08_2022__13_32_53", text: "Reporte 2"}, 
+    {value: "Julio Menchaca_18_08_2022__13_41_40", text: "Reporte 3"}
+]
+var list_of_SEFIN = [
+    {value: "Julio Menchaca_18_08_2022__11_15_02", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_18_08_2022__11_18_20", text: "Reporte 2"},
+    {value: "Julio Menchaca_18_08_2022__11_21_15", text: "Reporte 3"}, 
+    {value: "Julio Menchaca_18_08_2022__11_25_02", text: "Reporte 4"},
+    {value: "Julio Menchaca_18_08_2022__11_27_35", text: "Reporte 5"}
+]
+var list_of_SEGOBH= [
+    {value: "Julio Menchaca_18_08_2022__11_30_23", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_18_08_2022__11_32_51", text: "Reporte 2"},
+    {value: "Julio Menchaca_18_08_2022__11_34_50", text: "Reporte 3"}, 
+    {value: "Julio Menchaca_18_08_2022__11_45_19", text: "Reporte 4"},
+    {value: "Julio Menchaca_18_08_2022__11_47_19", text: "Reporte 5"}, 
+    {value: "Julio Menchaca_18_08_2022__12_55_25", text: "Reporte 6"}, 
+    {value: "Julio Menchaca_19_08_2022__00_39_39", text: "Reporte 7"}
+]
+var list_of_SEMARNATH = [
+    {value: "Julio Menchaca_18_08_2022__12_52_38", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_18_08_2022__13_15_00", text: "Reporte 2"}
+]
+var list_of_SEMOTH = [
+    {value: "Julio Menchaca_18_08_2022__12_23_56", text: "Reporte 1"}
+]
+var list_of_SEP = [
+    {value: "Julio Menchaca_18_08_2022__12_31_00", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_18_08_2022__12_35_22", text: "Reporte 2"},
+    {value: "Julio Menchaca_18_08_2022__13_21_49", text: "Reporte 3"}, 
+    {value: "Julio Menchaca_18_08_2022__13_26_00", text: "Reporte 4"},
+    {value: "Julio Menchaca_18_08_2022__14_24_04", text: "Reporte 5"}, 
+    {value: "Julio Menchaca_19_08_2022__01_30_14", text: "Reporte 6"}, 
+    {value: "Julio Menchaca_19_08_2022__01_37_57", text: "Reporte 7"}
+]
+var list_of_SEPPEH = [
+    {value: "Julio Menchaca_18_08_2022__12_10_21", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_18_08_2022__13_39_03", text: "Reporte 2"}
+]
+var list_of_SOPOT = [
+    {value: "Julio Menchaca_18_08_2022__13_46_55", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_19_08_2022__01_48_02", text: "Reporte 2"}
+]
+var list_of_STPS = [
+    {value: "Julio Menchaca_18_08_2022__12_41_11", text: "Reporte 1"}, 
+    {value: "Julio Menchaca_19_08_2022__01_54_20", text: "Reporte 2"}, 
+    {value: "Julio Menchaca_19_08_2022__01_59_03", text: "Reporte 3"}
+]
 var search_by_secretary = document.getElementById("search_by_secretary");
 var select_pdf = document.getElementById("select_pdf");
 var report_options = document.getElementById("report_options");
@@ -22,11 +130,53 @@ search_by_secretary.addEventListener("change", (e) => {
     }
     switch (search_by_secretary.value) {
         case "0":
-            add_options_to_select(report_options, list_of_health_secretary);
+            add_options_to_select(report_options, list_of_health_CONTRALORIA);
             break;
         case "1":
-            add_options_to_select(report_options, list_of_education_secretary);
-            break
+            add_options_to_select(report_options, list_of_education_GENERAL);
+            break;
+        case "2":
+            add_options_to_select(report_options, list_of_SA);
+            break;
+        case "3":
+            add_options_to_select(report_options, list_of_SECTUR);
+            break;
+        case "4":
+            add_options_to_select(report_options, list_of_SECULTAH);
+            break;
+        case "5":
+            add_options_to_select(report_options, list_of_SEDAGRO);
+            break;
+        case "6":
+            add_options_to_select(report_options, list_of_SEDECO);
+            break;
+        case "7":
+            add_options_to_select(report_options, list_of_SEDSO);
+            break;
+        case "8":
+            add_options_to_select(report_options, list_of_SEFIN);
+            break;
+        case "9":
+            add_options_to_select(report_options, list_of_SEGOBH);
+            break;
+        case "10":
+            add_options_to_select(report_options, list_of_SEMARNATH);
+            break;
+        case "11":
+            add_options_to_select(report_options, list_of_SEMOTH);
+            break;
+        case "12":
+            add_options_to_select(report_options, list_of_SEP);
+            break;
+        case "13":
+            add_options_to_select(report_options, list_of_SEPPEH);
+            break;
+        case "14":
+            add_options_to_select(report_options, list_of_SOPOT);
+            break;
+        case "15":
+            add_options_to_select(report_options, list_of_STPS);
+            break;
     }
     if (search_by_secretary.value !== "NA") {
         report_options.disabled = false;
@@ -39,6 +189,6 @@ search_by_secretary.addEventListener("change", (e) => {
 
 report_options.addEventListener("change", (e) => {
     report_options.value !== "NA" ?
-        show_pdf.src = "./assets/pdf/" + report_options.value +".pdf" :
+        show_pdf.src = "./assets/reportes/" + search_by_secretary.selectedOptions[0].text + "/" + report_options.value +".pdf" :
         show_pdf.src = "";
 });
